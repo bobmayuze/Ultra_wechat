c578996d1241d6abdad41f2094d63c6190c11d11
Yuze.Ma <584653629@qq.com>
2017-06-30 18:00:03 +0800

    Finish up meme_maker and user can send any msg to get a meme back lol

--- meme_maker.py
+++ meme_maker.py
@@ -1,13 +1,26 @@
+#!/usr/bin/env python
+#coding: utf-8
 import PIL
 from PIL import ImageFont, Image, ImageDraw
+import random 
 
-font = ImageFont.truetype("/Users/user/Documents/Projects/Ultra_Memer/chinese_font.ttf", 48)
-img = Image.open("greenT.png")
-# img=Image.new("RGBA", (200,200),(120,20,20))
-draw = ImageDraw.Draw(img)
-draw.text((200, 200),"你说啥",(43,43,43),font=font)
-draw = ImageDraw.Draw(img)
-draw = ImageDraw.Draw(img)
-img.save("a_test.png")
+def get_meme():
+	font = ImageFont.truetype("/Users/user/Documents/Projects/Ultra_wechat/chinese_font.ttf", 22)
+	img = Image.open("p.png")
 
-# target_img = Image.open("greenT.png")
+	# Find out the height and width of the target pic
+	width = img.size[0]
+	height = img.size[1]
+	msg_l = ["上车么骚年","上车吧骚年","那你很棒哦","放学后别走","编程猫听过么", "阿不就好棒棒"]
+	index = random.randint(0,len(msg_l)-1)
+
+	msg = msg_l[index]
+	dummy_msg = len(msg) * "**"
+
+
+	draw = ImageDraw.Draw(img)
+	tw, th = draw.textsize(dummy_msg)
+	draw.text(((width-tw)*0.4, height* 0.8),msg,(0,0,0),font=font)
+	img.save("out.png")
+
+get_meme()
